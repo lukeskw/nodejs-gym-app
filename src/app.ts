4753cfcc316e46dd42f8014e99b8a58db4213224
lukeskw <lucasporfirioa@gmail.com>
2024-02-15 19:08:44 -0300

    refactor: refactoring controllers splitting by functionality

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
-import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
+import { usersRoutes } from './http/controllers/users/routes'
+import { gymsRoutes, gymsRoutes } from './http/controllers/gyms/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
@@ -10,7 +12,9 @@ app.register(fastifyJwt, {
   secret: env.API_KEY,
 })
 
-app.register(appRoutes)
+app.register(usersRoutes)
+app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
