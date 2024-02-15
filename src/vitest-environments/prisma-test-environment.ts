c37bc1840ee2bcce7454b871748df0269b46117f
lukeskw <lucasporfirioa@gmail.com>
2024-02-15 18:13:46 -0300

    test: setup e2e environment for vitest

--- src/vitest-environments/prisma-test-environment.ts
+++ src/vitest-environments/prisma-test-environment.ts
@@ -1,14 +1,41 @@
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'web',
   async setup() {
-    console.log('setup')
+    const schema = randomUUID()
+
+    const databaseURL = generateDatabaseUrl(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
