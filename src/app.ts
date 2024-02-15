157b66da2cfe0acb07b7765f32abd00e79bbcae8
lukeskw <lucasporfirioa@gmail.com>
2024-02-15 20:17:10 -0300

    build: installing @fastify/cookies to handle the refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,26 @@
 import fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { ZodError } from 'zod'
 import { env } from './env'
 import { usersRoutes } from './http/controllers/users/routes'
-import { gymsRoutes, gymsRoutes } from './http/controllers/gyms/routes'
+import { gymsRoutes } from './http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.API_KEY,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '30m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
