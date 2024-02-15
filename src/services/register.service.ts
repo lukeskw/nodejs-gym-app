e6dc855b1abb45f64343cf9876fab22e3ad1fd91
lukeskw <lucasporfirioa@gmail.com>
2024-02-14 23:50:11 -0300

    test: creating first unit test

--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -1,6 +1,7 @@
 import { IUserRepository } from '@/repositories/iusers.repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsException } from './exceptions/user-already-exists.exception'
+import { User } from '@prisma/client'
 
 interface IRegisterInput {
   name: string
@@ -8,14 +9,22 @@ interface IRegisterInput {
   password: string
 }
 
+interface IUserResponse {
+  user: User
+}
+
 interface IRegisterService {
-  execute: ({ name, email, password }: IRegisterInput) => Promise<void>
+  execute: ({ name, email, password }: IRegisterInput) => Promise<IUserResponse>
 }
 
 export async function registerService(
   usersRepository: IUserRepository,
 ): Promise<IRegisterService> {
-  async function execute({ name, email, password }: IRegisterInput) {
+  async function execute({
+    name,
+    email,
+    password,
+  }: IRegisterInput): Promise<IUserResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await usersRepository.findByEmail(email)
@@ -23,11 +32,14 @@ export async function registerService(
       throw new UserAlreadyExistsException()
     }
 
-    await usersRepository.create({
+    const user = await usersRepository.create({
       name,
       email,
       password_hash,
     })
+    return {
+      user,
+    }
   }
   return {
     execute,
--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,38 @@
-import { expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { registerService } from './register.service'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(true).toBe(true)
+describe('Register Service', () => {
+  it('should hash the user password upon registration', async () => {
+    // as unit tests go, we should not need to implement the userRepository, because this type of test should not depend on databases
+    // const usersRepository = PrismaUsersRepository()
+
+    const register = await registerService({
+      async findByEmail() {
+        return null
+      },
+      async create(data) {
+        return {
+          id: crypto.randomUUID().toString(),
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+          updated_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await register.execute({
+      name: 'User Test',
+      email: 'userTest@prisma.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
